769dda6a0f9117b4f69900f895c4410f8b6a7989
sashaaero <sashaaero@yandex.ru>
2016-05-26 01:19:25 +0300

    create

---
+++ frame.py
@@ -0,0 +1,78 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget,
+                             QLabel, QLineEdit, QDateEdit, QCalendarWidget, QGridLayout)
+
+from PyQt5.QtGui import QIntValidator
+from license import License
+from datetime import date
+
+
+class Frame(QWidget):
+    FIND = True
+    EDIT = False
+
+    @staticmethod
+    def parse_date(input_str):
+        y, m, d = reversed(list(map(int, input_str.split('.'))))
+        return date(y, m, d)
+
+    def __init__(self, desktop):
+        super().__init__()
+        self.initGUI(desktop)
+        self.mode = self.FIND
+
+    def initGUI(self, desktop):
+        self.setGeometry(
+            desktop.width() * 0.1,
+            desktop.height() * 0.1,
+            desktop.width() * 0.8,
+            desktop.height() * 0.8
+        )
+        self.setWindowTitle('Контроль лицензий')
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        fields = License.get_fields()
+        self.input_forms = {}
+        # Делаем словарь, чтобы потом мы могли изменять данные
+
+        i = 1
+        for field in fields:
+            label = QLabel(field.rus)
+            if field.type == str:
+                self.input_forms[field.eng] = QLineEdit()
+                if 'лицензионный ключ' in field.rus.lower():
+                    self.input_forms[field.eng].setInputMask('>NNNNN-NNNNN-NNNNN-NNNNN-NNNNN;_')
+
+            elif field.type == int:
+                self.input_forms[field.eng] = QLineEdit()
+                if 'год' in field.rus.lower():
+                    self.input_forms[field.eng].setValidator(QIntValidator(1900, 2016))
+                    self.input_forms[field.eng].setMaxLength(4)
+            elif field.type == date.today:
+                self.input_forms[field.eng] = QDateEdit()
+                self.input_forms[field.eng].setCalendarPopup(True)
+
+            grid.addWidget(label, i, 0)
+            grid.addWidget(self.input_forms[field.eng], i, 1)
+            i += 1
+
+        self.setLayout(grid)
+
+        start = self.input_forms['delivery_date'].dateTime()
+        stop = self.input_forms['expiration_date'].dateTime()
+
+        if start <= stop:
+            print('Не выглядит правдой то, что лицензия истекает до ее активации')
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    desktop = app.desktop().screenGeometry()
+    frame = Frame(desktop)
+    frame.show()
+    sys.exit(app.exec_())
+
+main()
---
+++ license.py
@@ -0,0 +1,65 @@
+from datetime import date
+import sqlite3
+from collections import namedtuple
+
+field_model = namedtuple('field_model', ['eng', 'rus', 'type'])
+
+
+class License(dict):
+    fields = [
+        field_model('name',             'п.1 Наименование ПО',                                              str),
+        field_model('year',             'п.2 Год поставки',                                                 int),
+        field_model('id',               'п.3 № Лицензии',                                                   int),
+        field_model('developer',        'п.4 Разработчик',                                                  str),
+        field_model('firm',             'п.5 Фирма поставщик',                                              str),
+        field_model('delivery_date',    'п.6 Дата выдачи лицензии',                                         date.today),
+        field_model('expiration_date',  'п.7 Дата истечения периода программного сопровождения ',           date.today),
+        field_model('license_key',      'п.8 Лицензионный ключ',                                            str),
+        field_model('letter',           'п.8а Лицензионный договор или соответствующее письмо',             str),
+        field_model('note',             'п.8б Примечание',                                                  str),
+        field_model('distribution',     'п.9 Распределение лицензий',                                       str),
+        field_model('redistribution',   'п.10 Перераспределение лицензий',                                  str),
+        field_model('person',           'п.11 ФИО сотрудника, ответственного за использование лицензии',    str),
+        field_model('ip',               'п.12 ip-адрес',                                                    str),
+        field_model('server',           'п.13 Имя сервера',                                                 str),
+        field_model('task',             'п.14 Выполняемая задача',                                          str),
+        field_model('act',              'п.15 Акт об установке системы Windows',                            str),
+        field_model('department',       'п.16 Наименование отдела',                                         str)
+    ]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for field in self.fields:
+            self[field.eng] = field.type()
+
+    def write(self):
+        connection = sqlite3.connect('licenses.db')
+        with connection:
+            cursor = connection.cursor()
+            check_q = 'SELECT name FROM sqlite_master WHERE type="table" AND name="licenses";'
+            response = cursor.execute(check_q)
+            if len(response.fetchall()) == 0:
+                # надо создать базу
+                create_q = 'CREATE TABLE licenses (' + ', '.join([field.eng
+                                                                  if field.eng != 'id' else 'id INTEGER PRIMARY KEY'
+                                                                  for field in self.fields]) + ')'
+                cursor.execute(create_q)
+                connection.commit()
+
+            insert_q = 'INSERT INTO licenses VALUES (' + '?, ' * (len(self.fields) - 1) + '?)'
+            cursor.execute(insert_q, tuple(self[field.eng] for field in self.fields))
+
+    @staticmethod
+    def read(attributes):
+        connection = sqlite3.connect('licenses.db')
+        with connection:
+            cursor = connection.cursor()
+            keys = list(attributes.keys())
+            select_q = 'SELECT * FROM licenses WHERE ' + ' and '.join([key + '=?' for key in keys])
+            result = cursor.execute(select_q, tuple(attributes[key] for key in keys))
+
+        return result.fetchall()
+
+    @staticmethod
+    def get_fields():
+        return License.fields
