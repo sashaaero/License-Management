f568c4b90651f1aebfe25c804a6b240c571bfed9
sashaaero <sashaaero@yandex.ru>
2016-06-09 00:04:55 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+import license
+from frame import Frame
+
+def main():
+    app = QApplication(sys.argv)
+    desktop = app.desktop().screenGeometry()
+    frame = Frame(desktop)
+    frame.show()
+    sys.exit(app.exec_())
+
+main()
